7fa60c4d03a6b11e60e9c0efec6bc4f19dcf614e
mhakan <m.hakan@live.com>
2018-03-27 10:27:26 +0300

    Recursive exponentiation

---
+++ recursiveExp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+float ust(int x, int y);
+
+int main(){
+    float s;
+    s = ust(2,3);
+    cout<<s;
+     return 1;
+}
+
+float ust (int x, int y){
+    float sonuc;
+    cout<<"\nx = "<<x<<"\ty = "<< y<<"\n";
+    if (y<=1){
+        sonuc = x;
+        return sonuc;
+    }
+    else{
+        sonuc = x*ust(x,y-1);
+        return  sonuc;
+    }
+
+}
